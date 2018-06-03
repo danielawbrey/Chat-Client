b05f4a9f1f52a2217152fca9b2ad5816ff29494d
danielawbrey <danielawbrey@gmail.com>
2018-06-02 17:06:26 -0700

    Create foundational GUI for the client

--- src/main/resources/static/Time.js
+++ src/main/resources/static/Time.js
@@ -1,11 +1,14 @@
-function getTime() {
+$(document).ready(function printTime() {
+  var date = new Date();
+  document.getElementById("currentTime").innerText = date;
+  setInterval(printTime, 500);
+});
 
-    var date = new Date();
-    var hour = date.getHours();
-
-    if(hour < 12) { document.write("Good Morning").innerHTML; }
-    else if(hour >= 12 && hour < 17) { document.write("Good Afternoon").innerHTML; }
-    else { document.write("Good Evening").innerHTML; }
-
-    startTime();
-}
+$(document).ready(function getGreeting() {
+  var date = new Date();
+  var hour = date.getHours();
+  if(hour < 12) { document.getElementById("greeting").innerText = "Good Morning"; }
+  else if(hour >= 12 && hour < 17) { document.getElementById("greeting").innerText = "Good Afternoon"; }
+  else { document.getElementById("greeting").innerText = "Good Evening"; }
+  setInterval(getGreeting, 500);
+});
